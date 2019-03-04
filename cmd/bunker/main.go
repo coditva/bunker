17207e06d93af6f8546f0b214d00cfe3839483eb
Utkarsh Maheshwari <utkarshme96@gmail.com>
2019-03-05 04:31:45 +0530

    Add a basic logger

--- cmd/bunker/main.go
+++ cmd/bunker/main.go
@@ -1,7 +1,19 @@
 package main
 
 import "fmt"
+import (
+    "github.com/google/logger"
+    "os"
+)
 
 func main() {
+    var logger_out = os.Stdout
+    defer logger.Init("defaultLogger", false, true, logger_out).Close()
+
     fmt.Printf("CLI for the bunker\n")
+
+    logger.Info("Info")
+    logger.Warning("Warning")
+    logger.Error("Error")
+    logger.Fatal("Fatal")
 }
