48ad1bfeef3562940425cb4cb49fe4c204489176
Utkarsh Maheshwari <coditva@gmail.com>
2019-04-17 13:47:05 +0530

    Start containerd daemon on "/run/bunker/bunkerd.sock"

--- bunkerd.go
+++ bunkerd.go
@@ -1,7 +1,74 @@
 package main
 
 import "fmt"
+import (
+    "os"
+    "os/exec"
+    "syscall"
+)
+
+var helpText =
+`Usage:
+start
+stop
+restart
+enable
+disable
+`
+
+func start() error {
+    binary, lookErr := exec.LookPath("containerd")
+    if lookErr != nil {
+        fmt.Println("Could not find containerd")
+        os.Exit(1)
+    }
+    args := []string{"containerd", "--address", "/run/bunker/bunkerd.sock",
+            "--log-level", "fatal"}
+    env := os.Environ()
+
+    procAttr := syscall.ProcAttr{
+        Dir: "/tmp",
+        Env: env,
+    }
+
+    pid, err := syscall.ForkExec(binary, args, &procAttr)
+    fmt.Println("Daemon Process: ", pid)
+    return err
+}
+
+func stop() error {
+    return nil
+}
+
+func enable() error {
+    return nil
+}
+
+func disable() error {
+    return nil
+}
+
+func printHelp() {
+    fmt.Printf(helpText)
+}
 
 func main() {
-    fmt.Printf("Contain this!\n")
+    if len(os.Args) < 2 {
+        printHelp()
+        os.Exit(1)
+    } else if os.Args[1] == "start" {
+        start()
+    } else if os.Args[1] == "stop" {
+        stop()
+    } else if os.Args[1] == "enable" {
+        enable()
+    } else if os.Args[1] == "disable" {
+        disable()
+    } else if os.Args[1] == "restart" {
+        stop()
+        start()
+    } else {
+        printHelp()
+        os.Exit(1)
+    }
 }
