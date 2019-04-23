fb7e88ba37d05683e23a7aa96bd367e66af7872f
Utkarsh Maheshwari <coditva@gmail.com>
2019-04-23 17:19:09 +0530

    Add labels to listing commands
    (fix #12)

--- internal/api/images.go
+++ internal/api/images.go
@@ -4,6 +4,7 @@ import (
     "fmt"
 
     lib "github.com/coditva/bunker/internal"
+    util "github.com/coditva/bunker/internal/util"
     types "github.com/coditva/bunker/internal/types"
 )
 
@@ -14,6 +15,7 @@ func (api Api) Images(args *types.Args, reply *string) error {
         return err
     }
 
+    *reply = "Size\tImage\n------\t--------------"
     for _, image := range images{
         name := image.Name()
         size, err := image.Size(lib.ContainerdClient.Ns)
@@ -21,7 +23,7 @@ func (api Api) Images(args *types.Args, reply *string) error {
             lib.Logger.Warning("Unknown size for image ", name, ": ", err)
             size = 0
         }
-        *reply = fmt.Sprintf("%v%v\t%v\n", *reply, name, size)
+        *reply = fmt.Sprintf("%v\n%v\t%v", *reply, util.SizeToString(size), name)
     }
 
     return nil
